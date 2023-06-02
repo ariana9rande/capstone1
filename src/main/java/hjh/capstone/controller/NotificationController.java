7e05146d1092ee6631ac38148bc5082aa10eaf6e
hjh970422 <hjh970422@gmail.com>
2023-06-03 02:11:05 +0900

    230603

--- src/main/java/hjh/capstone/controller/NotificationController.java
+++ src/main/java/hjh/capstone/controller/NotificationController.java
@@ -5,8 +5,10 @@ import hjh.capstone.domain.notification.Notification;
 import hjh.capstone.domain.wait.Wait;
 import hjh.capstone.service.NotificationService;
 import hjh.capstone.service.WaitService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,32 +19,15 @@ public class NotificationController
     private final WaitService waitService;
     private final SimpMessagingTemplate simpMessagingTemplate;
     private final NotificationService notificationService;
+    private Notification notification;
 
-    public NotificationController(WaitService waitService, SimpMessagingTemplate simpMessagingTemplate, NotificationService notificationService)
+    public NotificationController(WaitService waitService, SimpMessagingTemplate simpMessagingTemplate,
+                                  NotificationService notificationService)
     {
         this.waitService = waitService;
         this.simpMessagingTemplate = simpMessagingTemplate;
         this.notificationService = notificationService;
     }
-
-//    @GetMapping("/{restId}/notice")
-//    public String sendNotification(@PathVariable Long restId, @RequestParam Long waitId)
-//    {
-//        Wait wait = waitService.findById(waitId);
-//        Member member = wait.getMember();
-//
-//        String title = "입장 안내";
-//        String body = "대기번호 " + wait.getWaitNumber() + "번 입장 5분 전입니다.";
-//        String icon = "/images/favicon.ico";
-//
-//        Notification notification = new Notification(title, body, icon);
-//
-//        simpMessagingTemplate.convertAndSendToUser(member.getMemberId().toString(), "/queue/notifications", notification);
-////        fcmService.sendNotification(member.getToken(), title, body);
-////        System.out.println("NotificationController : fcmService.sendNotification 호출 완료");
-//
-//        return "redirect:/" + restId + "/manage";
-//    }
 
     @PostMapping("/{restId}/notice")
     public String sendNotification(@PathVariable Long restId, @RequestParam Long waitId)
@@ -50,18 +35,31 @@ public class NotificationController
         Wait wait = waitService.findById(waitId);
         Member member = wait.getMember();
 
-        System.out.println("postMapping 완료");
+        System.out.println("/" + restId + "/notice" + " postMapping 완료");
 
         String title = "입장 안내";
         String body = "대기번호 " + wait.getWaitNumber() + "번 입장 5분 전입니다.";
         String icon = "/images/favicon.ico";
 
-        Notification notification = new Notification(title, body, icon);
+        notification = new Notification(title, body, icon);
 
 //        notificationService.sendNotification(notification);
         simpMessagingTemplate.convertAndSend("/notifications", notification);
+        System.out.println("notification = " + notification);
 
         return "redirect:/" + restId + "/manage";
     }
 
+    @GetMapping("/notifications")
+    public ResponseEntity<Notification> getNotification()
+    {
+        if (notification != null)
+        {
+            return ResponseEntity.ok(notification); // 전역 변수 반환
+        }
+        else
+        {
+            return ResponseEntity.notFound().build(); // 알림이 없을 경우 404 에러 반환
+        }
+    }
 }
--- src/main/resources/static/js/service-worker.js
+++ src/main/resources/static/js/service-worker.js
@@ -15,15 +15,24 @@ self.addEventListener('push', function(event) {
                     icon: icon
                 };
                 console.log("data : " + data);
-                return self.registration.showNotification(title, options);
+                return self.showNotification(title, options);
+            })
+            .catch(function(error) {
+                console.error('Error sending notification:', error);
             })
     );
 });
 
-// 1초마다 알림 확인
+//1초마다 알림 확인
 setInterval(function() {
     self.dispatchEvent(new Event('push')); // push 이벤트 디스패치
 }, 1000);
+
+function showNotification(title, body, icon) {
+    if ('Notification' in window && Notification.permission === 'granted') {
+        new Notification(title, { body: body, icon: icon });
+    }
+}
 
 // self.addEventListener("push", (event) => {
 //     const payload = JSON.parse(event.data.text());
